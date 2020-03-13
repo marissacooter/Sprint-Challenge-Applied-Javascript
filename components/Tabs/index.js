1972afe816d9f543fc86979a38301773d90d9a78
Marissa <marissacooterl@gmail.com>
2020-03-13 11:09:08 -0500

    Finished making tabs component. Checking results in browser.

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,26 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+
+// AXIOS REQUEST:
+
+const myTabsParent = document.querySelector(div.cards);
+
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+    .then(repsonse => {
+        console.log(response);
+        myTabsParent.append(tabsComp(response.data))
+    })
+    .catch(error =>{
+        console.log('The data was not returned', error);
+    })    
+
+
+// MY FUNCTION:
+
+function tabsComp (object){
+    const tab = document.createElement('div');
+    tab.classList.add('tab');
+    tab.textContent = 'Topic Here';
+}
